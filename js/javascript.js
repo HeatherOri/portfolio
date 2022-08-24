6884589d18afae14fb4f570dceb23406206e67a3
Heather Liu <chihying512@gmail.com>
2022-08-24 11:32:35 +0930

    add the JS function and set up open graph

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+var anchors = document.getElementsByName("a");
+
+if(anchors){
+anchors[0].addEventListener("click",function(){document.getElementById("openNavbar").checked = false;});
+anchors[1].addEventListener("click",function(){document.getElementById("openNavbar").checked = false;});
+anchors[2].addEventListener("click",function(){document.getElementById("openNavbar").checked = false;});
+anchors[3].addEventListener("click",function(){document.getElementById("openNavbar").checked = false;});
+anchors[4].addEventListener("click",function(){document.getElementById("openNavbar").checked = false;});
+}
